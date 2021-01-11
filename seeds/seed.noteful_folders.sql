2452e7b5ab87d04b116532577997f25971b36dc9
Chad Moravec <chad@pop-os.localdomain>
2021-01-10 18:20:55 -0700

    Routes for CRUD functionality...

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,4 @@
+INSERT INTO folders (name)
+VALUES
+('Random'),
+('Stuff');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,4 @@
+INSERT INTO notes (name, folder_id, content)
+VALUES
+('First Note!', 1, 'This is the first note...'),
+('Second Note!', 2, 'This is the second note...');

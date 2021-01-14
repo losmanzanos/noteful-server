179b3775657ecccf09623567594214b7a8ce3518
Chad Moravec <moravec@colorado.edu>
2021-01-13 17:49:21 -0700

    fixed migrations

--- migrations/002.do.create_noteful_notes.sql
+++ migrations/002.do.create_noteful_notes.sql
@@ -5,6 +5,6 @@ CREATE TABLE notes (
     name TEXT NOT NULL,
     modified TIMESTAMP DEFAULT now() NOT NULL,
     folder_id INTEGER
-        REFERENCES folders(id) ON DELETE CASCADE NOT NULL,
+    REFERENCES folders(id) ON DELETE CASCADE NOT NULL,
     content TEXT NOT NULL
 );
